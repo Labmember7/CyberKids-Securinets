0bb3962f8e8d1dee171d98e138f0e9a8717e9379
Bacem <bacem.zarai@insat.u-carthage.tn>
2020-12-23 19:26:22 +0100

    first meeting tuto

---
+++ Assets/OnClickChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickChange : MonoBehaviour
+{
+    public GameObject textObject;
+    public string chaine;
+    public void ChangeTextOnClick()
+    {
+        textObject.GetComponent<TextMesh>().text = chaine;
+    }
+}
