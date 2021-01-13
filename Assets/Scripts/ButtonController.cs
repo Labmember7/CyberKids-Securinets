6677c6279969bc2ac7f3d5021212106df015dcb9
Bacem <bacem.zarai@insat.u-carthage.tn>
2021-01-13 22:18:41 +0100

    Beautified the look
    Scripts need to be merged and optimised into one script containing all the functions

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+   public void EmitParticles(GameObject particles)
+    {
+
+        StartCoroutine(EmitForSeconds(particles));
+    }
+
+    IEnumerator EmitForSeconds(GameObject particles)
+    {
+
+            UIParticleSystem particleSystem = particles.GetComponent<UIParticleSystem>();
+            particleSystem.enabled = true;
+            yield return null;
+            particleSystem.enabled = false;
+
+
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex)+1);
+    }
+}
---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.up, 20 * Time.deltaTime);
+
+    }
+}
