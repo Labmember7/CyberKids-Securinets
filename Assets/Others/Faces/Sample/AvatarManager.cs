2acaf677d03c73439b855db59a50930aee28628c
Bacem <bacem.zarai@insat.u-carthage.tn>
2021-01-18 16:43:36 +0100

    Setting up login/Register UI

---
+++ Assets/Others/Faces/Sample/AvatarManager.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvatarManager : MonoBehaviour {
+	public Image cbody;
+	public Image cface;
+	public Image chair;
+	public Image ckit;
+	public Sprite[] body;
+	public Sprite[] face;
+	public Sprite[] hair;
+	public Sprite[] kit;
+	public Color[] background;
+
+	public int iHair = 0;
+	public int iBody = 0;
+	public int iFace = 0;
+	public int iKit = 0;
+	public int iBgColor = 0;
+	public Avatar avatar =new Avatar();
+	public GameObject panel;
+	// Use this for initialization
+	void Start () {
+		//cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+		//RandomizeCharacter();
+		//SetAvatar();
+
+		//Wait For Avatar Data from server
+
+
+	}
+	
+	public void RandomizeCharacter(){
+		iBody = Random.Range(0, body.Length);
+		iBgColor = Random.Range(0, background.Length);
+		iFace = Random.Range(0, face.Length);
+		iHair = Random.Range(0, hair.Length);
+		iKit = Random.Range(0, kit.Length);
+
+		cbody.sprite = body[iBody];
+		cface.sprite = face[iFace];
+		chair.sprite = hair[iHair];
+		ckit.sprite = kit[iKit];
+		panel.GetComponent<RawImage>().color = background[iBgColor];
+		
+	}
+	
+    public void SetAvatar()
+    {
+		avatar.bgColor = iBgColor.ToString();
+		avatar.face = iFace.ToString();
+		avatar.hair = iHair.ToString();
+		avatar.body = iBody.ToString();
+		avatar.kit = iKit.ToString();
+	}
+
+	public void GetAvatar()
+	{
+		iBgColor =int.Parse(avatar.bgColor);
+		iFace =int.Parse(avatar.face);
+		iHair =int.Parse(avatar.hair);
+		iBody =int.Parse(avatar.body);
+		iKit = int.Parse(avatar.kit);
+
+		chair.sprite = hair[iHair];
+		cbody.sprite = body[iBody];
+		cface.sprite = face[iFace];
+		ckit.sprite = kit[iKit];
+		panel.GetComponent<RawImage>().color = background[iBgColor];
+	}
+
+
+
+	//Change Hair
+
+	public void NextHair(){
+
+		iHair = (++iHair) % hair.Length;
+		chair.sprite = hair[iHair];
+
+	}
+	public void PreviousHair()
+	{
+		--iHair;
+		if(iHair<0)
+        {
+			iHair = hair.Length - 1;
+		}
+		chair.sprite = hair[iHair];
+	}
+
+	//Change Body
+
+	public void NextBody()
+	{
+
+		iBody = (++iBody) % body.Length;
+
+		cbody.sprite = body[iBody];
+
+	}
+	public void PreviousBody()
+	{
+		--iBody;
+		if (iBody < 0)
+		{
+			iBody = body.Length - 1;
+		}
+		cbody.sprite = body[iBody];
+	}
+
+	//Change Face
+	public void NextFace()
+	{
+
+		iFace = (++iFace) % face.Length;
+
+		cface.sprite = face[iFace];
+
+	}
+	public void PreviousFace()
+	{
+		--iFace;
+		if (iFace < 0)
+		{
+			iFace = face.Length - 1;
+		}
+		cface.sprite = face[iFace];
+	}
+	//Change Kit
+	public void NextKit()
+	{
+
+		iKit = (++iKit) % kit.Length;
+
+		ckit.sprite = kit[iKit];
+
+	}
+	public void PreviousKit()
+	{
+		--iKit;
+		if (iKit < 0)
+		{
+			iKit = kit.Length - 1;
+		}
+		ckit.sprite = kit[iKit];
+	}
+	//Change Background color
+	public void NextBgColor()
+	{
+
+		iBgColor = (++iBgColor) % background.Length;
+		panel.GetComponent<RawImage>().color = background[iBgColor];
+
+	}
+	public void PreviousBgColor()
+	{
+		--iBgColor;
+		if (iBgColor < 0)
+		{
+			iBgColor = background.Length - 1;
+		}
+		panel.GetComponent<RawImage>().color = background[iBgColor];
+	}
+
+
+
+}
+
--- Assets/Scripts/PlayfabManager.cs
+++ Assets/Scripts/PlayfabManager.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using PlayFab;
 using PlayFab.ClientModels;
 
 public class PlayfabManager : MonoBehaviour
 {
+    public Avatar avatar;
+    public bool responseReceived = false;
+    public GameObject avatarObject;
+    public GameObject loadingPanel;
+    public GameObject errorPanel;
+    public GameObject username;
+
     // Start is called before the first frame update
     void Start()
     {
         Login();
-    }
 
-    void Login()
+        //Reference the avatar data
+        avatar = avatarObject.GetComponent<AvatarManager>().avatar;
+    }
+    public void Login()
     {
         var request = new LoginWithCustomIDRequest
         {
@@ -20,16 +30,24 @@ public class PlayfabManager : MonoBehaviour
             CreateAccount = true
         };
         PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnFailure);
+        StartCoroutine(WaitForResponse());
+
 
     }
     void OnSuccess(LoginResult result)
     {
         Debug.Log("Successful Login "+ result.PlayFabId);
+        GetAvatar();
+        //responseReceived = true;
+
     }
     void OnFailure(PlayFabError error)
     {
         Debug.Log("Error while loggin in Check network");
         Debug.Log(error.GenerateErrorReport());
+        responseReceived = true;
+        //Show error on screen => activation error panel
+        errorPanel.SetActive(true);
 
     }
 
@@ -75,4 +93,86 @@ public class PlayfabManager : MonoBehaviour
         }
 
     }
+    public void GetAvatar()
+    {
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest(), OnDataReceived, OnFailure);
+        StartCoroutine(WaitForResponse());
+
+    }
+
+    void OnDataReceived(GetUserDataResult result)
+    {
+        Debug.Log("Reveived User Data");
+        if(result.Data != null && result.Data["username"].Value!=null)
+        {
+            avatar.bgColor = result.Data["bgColor"].Value;
+            avatar.hair = result.Data["hair"].Value;
+            avatar.face = result.Data["face"].Value;
+            avatar.body = result.Data["body"].Value;
+            avatar.kit = result.Data["kit"].Value;
+            avatarObject.GetComponent<AvatarManager>().avatar = avatar ;
+            avatarObject.GetComponent<AvatarManager>().GetAvatar() ;
+            username.GetComponent<InputField>().text = result.Data["username"].Value;
+            /*Debug.LogWarning(avatarObject.GetComponent<AvatarManager>().avatar.bgColor);
+            Debug.LogWarning(avatar.hair);
+            Debug.LogWarning(avatar.face);
+            Debug.LogWarning(avatar.body);
+            Debug.LogWarning(avatar.kit);*/
+        }
+        else
+        {
+            SetAvatar();
+        }
+        responseReceived = true;
+    }
+
+
+   public void SetAvatar()
+    {
+        avatar.username = username.GetComponent<InputField>().text;
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string>
+            {
+                {"face" , avatar.face },
+                { "hair",avatar.hair },
+                { "body",avatar.body },
+                { "kit",avatar.kit },
+                { "bgColor",avatar.bgColor },
+                { "username",avatar.username }
+
+            }
+        };
+        PlayFabClientAPI.UpdateUserData(request, OnDataSent, OnFailure);
+        StartCoroutine(WaitForResponse());
+
+    }
+    IEnumerator WaitForResponse()
+    {
+        loadingPanel.SetActive(true);
+        
+        while (!responseReceived)
+        {
+            //Debug.Log(Time.time);
+            yield return null;
+        }
+        responseReceived = false;
+        loadingPanel.SetActive(false);
+
+    }
+    void OnDataSent(UpdateUserDataResult result)
+    {
+        Debug.Log("Successful User Data Sent!");
+        responseReceived = true;
+    } 
+}
+public class Avatar
+{
+    public string face="0";
+    public string hair="0";
+    public string body="0";
+    public string kit="0";
+    public string bgColor="0";
+    public string username="Username...";
+    
 }
